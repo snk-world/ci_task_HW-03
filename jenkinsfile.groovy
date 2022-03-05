b86c502e38868a7e4a4fdf98ac24f66d864734b7
Your Name <you@example.com>
2022-03-05 19:14:02 +0000

    another commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,7 +7,6 @@ node('master') {
 	stage('Compile') {// Компиляция и выполнение модульного тестирования;
 		// Запуск Gradle для выполнения компиляции и модульного тестирования;
 		sh "echo 123"
-		sh "set +x; mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <script.sql"
 	}
 	stage('exit') {
 		sh 'echo end'
