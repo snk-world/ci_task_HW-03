735a813fca92297e0c04cd54ded8bb558ede7e83
ilia <ilia.vaster@gmail.com>
2022-03-10 00:52:43 +0300

    another commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,12 +1,10 @@
 // Сценарный конвейер;
 node('master') {
 	stage('Prepeare Source') {// Получение кода;
-		// Получаем код из нашего Git-репозитория;
 		git branch: 'main', url: 'https://github.com/snk-world/ci_task_HW-03'
 	}
-	stage('Update container') {// Компиляция и выполнение модульного тестирования;
-		// Запуск Gradle для выполнения компиляции и модульного тестирования;
-		sh "docker-compose up -d"
+	stage('Update container') {
+		sh "sudo docker-compose up -d"
 	}
 	stage('clear workspace') {
 		sh 'echo end'
