b9849c1a8a506018c648fbaf37f0b507216c8532
Your Name <you@example.com>
2022-03-10 00:14:47 +0000

    another commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,16 +8,30 @@ node('master') {
 		sh "docker-compose up -d"
 	}
 	stage('check status') {
+		def error = false
 		def response = sh(script: " curl -I http://localhost:9889 | grep HTTP | awk '{print \$2}'", returnStdout: true).trim()
 		echo response
 		if (response != '200') {
 			echo 'not ok'
+			error = true
 		} else {
 			echo 'ok'
-		} 	
+		} 
+		
+		def md5http= sh (script: "curl -sL http://localhost:9889/index.html | md5sum | cut -d ' ' -f 1", returnStdout:true).trim()	
+
+		def md5local= sh (script: "md5sum www/index.html | cut -d ' ' -f 1", returnStdout:true).trim()	
+		if (md5thhp != md5local){
+			echo 'not ok'
+			error = true
+		}
+		if (error=true) {
+			echo 'error'
+		}
+
 	}
 	stage('clear workspace') {
 		//sh 'docker-compose down'
-		sh 'echo end'
+		echo 'end'
 	}
 }
